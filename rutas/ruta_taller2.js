617a57c2c868952e31079e68e7ddac640dbcef8b
Conjunto-de-particulas <camilo.coc1228@gmail.com>
2025-11-10 15:01:53 -0500

    ruta taller 2

---
+++ rutas/ruta_taller2.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const {findMax, includes, sum, missingNumbers} = require('../taller-02')
+
+router.post('/findMax', (req, res) => {
+  const {lista_numeros} = req.body
+  if (!Array.isArray(lista)) {
+    return res.status(400).json({error: 'El elemento no es una lista'})
+  }
+  const resultado = findMax(lista_numeros)
+  res.json({resultado})
+})
+
+router.post('/includes', (req, res) => {
+  const {numero, lista_numeros} = req.body
+  if (!Array.isArray(lista) || numero === undefined) {
+    return res.status(400).json({error: 'Se requiere un numero y una lista'})
+  }
+  const resultado = includes(lista_numeros, numero)
+  res.json({resultado})
+})
+
+router.post('/sum', (req, res) => {
+  const {lista_numeros} = req.body
+  if (!Array.isArray(lista)) {
+    return res.status(400).json({error: 'El elemento no es una lista'})
+  }
+  const resultado = sum(lista_numeros)
+  res.json({resultado})
+})
+
+router.post('/missingNumbers', (req, res) => {
+  const {lista_numeros} = req.body
+  if (!Array.isArray(lista)) {
+    return res.status(400).json({error: 'El elemento no es una lista'})
+  }
+  const resultado = missingNumbers(lista_numeros)
+  res.json({resultado})
+})
+
+module.exports = router
